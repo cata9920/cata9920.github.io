c7359c0dbd9ba68a14745534e0e68941a4676a05
cata9920 <69439139+cata9920@users.noreply.github.com>
2024-11-07 12:46:40 -0500

    Update javascript.js

--- JSFolder/javascript.js
+++ JSFolder/javascript.js
@@ -5,13 +5,16 @@ var miniprojectsbox = document.getElementById("MiniProjects");
 var aboutmephoto = document.getElementById("aboutmephoto");
 var aboutmetextcontainer = document.getElementById("aboutmetextcontainer");
 
+var contactbox = document.getElementById("ContactMe");
+var contactcontainer = document.getElementById("contactcontainer");
+
 var x = document.getElementById("x");
 var o = document.getElementById("o");
 var y = document.getElementById("y");
 
-var project01 = document.getElementById("project01")
-var project02 = document.getElementById("project02")
-var project03 = document.getElementById("project03")
+var project01 = document.getElementById("project01");
+var project02 = document.getElementById("project02");
+var project03 = document.getElementById("project03");
 
 var textcontainer = document.getElementById("textcontainer");
 
@@ -26,7 +29,8 @@ y.addEventListener("click",showproject03);
 function aboutmefunction() {
 
     aboutmebox.setAttribute("style", "background-color: black;");
-    miniprojectsbox.setAttribute("style", "background-color: rgba(0,0,0,0.4);")
+    miniprojectsbox.setAttribute("style", "background-color: rgba(0,0,0,0.4);");
+    contactbox.setAttribute("style", "background-color: rgba(0,0,0,0.4);");
     aboutmephoto.setAttribute("style","visibility: initial");
     aboutmetextcontainer.setAttribute("style","visibility: initial");
 
@@ -44,6 +48,7 @@ function aboutmefunction() {
     project03.setAttribute("style","visibility: hidden");
 
     textcontainer.setAttribute("style","visibility: hidden");
+    contactcontainer.setAttribute("style","visibility: hidden");
 
 
     //shownoprojects();
@@ -51,13 +56,40 @@ function aboutmefunction() {
 
 aboutmebox.addEventListener("click",aboutmefunction);
 
+function contactmefunction() {
+    contactbox.setAttribute("style", "background-color: black;");
+    miniprojectsbox.setAttribute("style", "background-color: rgba(0,0,0,0.4);");
+    aboutmebox.setAttribute("style","background-color: rgba(0,0,0,0.4);");
+    contactcontainer.setAttribute("style","visibility: initial");
+    textcontainer.setAttribute("style","visibility: hidden");
+
+    spannr1.setAttribute("style","visibility: hidden");
+    spannr2.setAttribute("style","visibility: hidden");
+    spannr3.setAttribute("style","visibility: hidden");
+
+    x.setAttribute("style","visibility: hidden");
+    o.setAttribute("style","visibility: hidden");
+    y.setAttribute("style","visibility: hidden");
+
+    project01.setAttribute("style","visibility: hidden");
+    project02.setAttribute("style","visibility: hidden");
+    project03.setAttribute("style","visibility: hidden");
+
+    aboutmephoto.setAttribute("style","visibility: hidden");
+    aboutmetextcontainer.setAttribute("style","visibility: hidden");
+}
+
+contactbox.addEventListener("click",contactmefunction);
+
 
 function miniprojectsfunction() {
 
     aboutmebox.setAttribute("style", "background-color: rgba(0,0,0,0.4);");
-    miniprojectsbox.setAttribute("style", "background-color: black;")
+    contactbox.setAttribute("style", "background-color: rgba(0,0,0,0.4);");
+    miniprojectsbox.setAttribute("style", "background-color: black;");
     aboutmephoto.setAttribute("style","visibility: hidden");
     aboutmetextcontainer.setAttribute("style","visibility: hidden");
+    contactcontainer.setAttribute("style","visibility: hidden");
 
     x.setAttribute("style","visibility: initial");
     o.setAttribute("style","visibility: initial");
@@ -70,6 +102,15 @@ function miniprojectsfunction() {
    
 
     textcontainer.setAttribute("style","visibility: initial");
+
+    let project01property = window.getComputedStyle(project01);
+    let project02property = window.getComputedStyle(project02);
+    let project03property = window.getComputedStyle(project03);
+
+    if (project01property.visibility == "hidden" && project02property.visibility == "hidden" && project03property.visibility == "hidden" ) {
+        console.log("super");
+        project01.setAttribute("style","visibility: initial");
+    }
 }
 
 miniprojectsbox.addEventListener("click",miniprojectsfunction);
